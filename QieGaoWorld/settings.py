1278f73ae6334fb163f6a53a19f297b0fc8df218
difuer <3039067847@qq.com>
2019-10-16 19:58:40 +0800

    mysql

--- QieGaoWorld/settings.py
+++ QieGaoWorld/settings.py
@@ -122,9 +122,15 @@ WSGI_APPLICATION = 'QieGaoWorld.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'QieGaoWorld/my.conf')
+        }
     }
 }
 
